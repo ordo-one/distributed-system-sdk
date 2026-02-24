b55d2d4c0c94ccd37f566fdb31146e58f866ad39
Diana Ma <diana@ordo.one>
2026-02-24 19:42:56 +0000

    ic

---
+++ Package.template.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let tag: String = "__TAG__"
+var url: String {
+    let release: String = "https://github.com/ordo-one/internal-distributed-system/releases/download/\(tag)"
+    #if os(macOS)
+    return "\(release)/DistributedABI-macOS.xcframework.zip"
+    #else
+    return "\(release)/DistributedABI-Linux.xcframework.zip"
+    #endif
+}
+var checksum: String {
+    #if os(macOS)
+    "__CHECKSUM_MACOS__"
+    #else
+    "__CHECKSUM_LINUX__"
+    #endif
+}
+
+let package: Package = .init(
+    name: "internal-distributed-system",
+    products: [
+        .library(name: "DistributedABI", targets: ["DistributedABI"])
+    ],
+    targets: [
+        .binaryTarget(name: "DistributedABI", url: url, checksum: checksum),
+    ]
+)
